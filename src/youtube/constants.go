34bcc6fb03ad373fe08c0d6887a9a2526af6cb3f
streambinder <posta@davidepucci.it>
2017-06-04 18:51:40 +0200

    src: youtube: switch to standard query options
    Despite of what I was thinking, YouTube's pertinence query filter
    better fits into our needs, instead of filtering by views.

--- src/youtube/constants.go
+++ src/youtube/constants.go
@@ -2,7 +2,7 @@ package youtube
 
 const (
 	YOUTUBE_QUERY_URL      = "https://www.youtube.com/results"
-	YOUTUBE_QUERY_PATTERN  = YOUTUBE_QUERY_URL + "?sp=CAMSAhAB&q=%s"
+	YOUTUBE_QUERY_PATTERN  = YOUTUBE_QUERY_URL + "?q=%s"
 	YOUTUBE_VIDEO_SELECTOR = ".yt-uix-tile-link"
 	YOUTUBE_VIDEO_PREFIX   = "https://www.youtube.com"
 )

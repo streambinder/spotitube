88ccdbce88e32003bcd60762f30acf81131e2190
streambinder <posta@davidepucci.it>
2017-06-05 16:15:58 +0200

    src: aesthetics is important, too
    More specific status messages along with some graphical
    enhancements.

--- src/utils/common.go
+++ src/utils/common.go
@@ -1,8 +1,12 @@
 package utils
 
 import (
+	"fmt"
 	"os"
+	"runtime"
 	"strings"
+
+	"github.com/fatih/color"
 )
 
 // system utils
@@ -20,6 +24,52 @@ func IsDir(path string) bool {
 }
 
 // spotify-dl utils
+
+type Logger struct {
+	Prefix string
+	Color  func(a ...interface{}) string
+}
+
+func NewLogger() Logger {
+	var shell_color func(a ...interface{}) string = color.New(SHELL_COLOR_DEFAULT).SprintFunc()
+	var caller_package string = "unknown"
+	pc, _, _, ok := runtime.Caller(1)
+	details := runtime.FuncForPC(pc)
+	if ok && details != nil {
+		caller_package = strings.Split(details.Name(), ".")[0]
+		if caller_package == "main" {
+			shell_color = color.New(SHELL_COLOR_MAIN).SprintFunc()
+		} else if caller_package == "spotify" {
+			shell_color = color.New(SHELL_COLOR_SPOTIFY).SprintFunc()
+		} else if caller_package == "youtube" {
+			shell_color = color.New(SHELL_COLOR_YOUTUBE).SprintFunc()
+		}
+	}
+	logger := Logger{
+		Prefix: caller_package,
+		Color:  shell_color,
+	}
+	return logger
+}
+
+func (logger Logger) ColoredPrefix() string {
+	return logger.Color("[" + strings.ToUpper(logger.Prefix) + "]")
+}
+
+func (logger Logger) Log(message string, fatal ...bool) {
+	var is_fatal bool = (len(fatal) == 1 && fatal[0])
+	if is_fatal {
+		message = color.RedString(message)
+	}
+	fmt.Println(logger.ColoredPrefix(), message)
+	if is_fatal {
+		os.Exit(1)
+	}
+}
+
+func (logger Logger) Fatal(message string) {
+	logger.Log(message, true)
+}
 
 type Track struct {
 	Title  string
---
+++ src/utils/constants.go
@@ -0,0 +1,12 @@
+package utils
+
+import (
+	"github.com/fatih/color"
+)
+
+const (
+	SHELL_COLOR_DEFAULT = color.FgWhite
+	SHELL_COLOR_MAIN    = color.FgBlue
+	SHELL_COLOR_YOUTUBE = color.FgRed
+	SHELL_COLOR_SPOTIFY = color.FgGreen
+)

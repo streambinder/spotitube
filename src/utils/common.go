c992c74806af1c827b429a5cb3689dbac7c38086
streambinder <posta@davidepucci.it>
2017-06-09 14:59:59 +0200

    src: more accurate '&' replacing in songs name

--- src/utils/common.go
+++ src/utils/common.go
@@ -116,7 +116,7 @@ func (track Track) Normalize() Track {
 func (track Track) Seems(sequence string) bool {
 	sequence_sanitized := sanitize.Name(strings.ToLower(sequence))
 	track_title := strings.ToLower(track.Title)
-	track_title = strings.Replace(track_title, "&", "and", -1)
+	track_title = strings.Replace(track_title, " & ", "and", -1)
 	for _, splitter := range []string{" and ", "feat. "} {
 		if strings.Contains(track_title, splitter) {
 			track_title = strings.Split(track_title, splitter)[0]
@@ -125,7 +125,7 @@ func (track Track) Seems(sequence string) bool {
 	track_title = sanitize.Name(track_title)
 
 	track_artist := strings.ToLower(track.Artist)
-	track_artist = strings.Replace(track_artist, "&", "and", -1)
+	track_artist = strings.Replace(track_artist, " & ", " and ", -1)
 	for _, splitter := range []string{" and "} {
 		if strings.Contains(track_artist, splitter) {
 			track_artist = strings.Split(track_artist, splitter)[0]

ae9c5d31b80a6339baf1ea2691f136597f31e77b
streambinder <posta@davidepucci.it>
2017-12-14 18:13:21 +0100

    version: increase to v10
    This is an extremely important release, which includes:
     - brand new graphical interface, based on @jroimartin library, `gocui`
     - brand new logging/gui libraries built from scratch
     - gui (and logger, then) library being interacted only by main module, without spamming other libraries code with things not belonging to their logic and making them completely independent
     - brand new threadpool used to speed up things on normalizing audio and fixing metadata, without that bad error about `too much open files` when large libraries were synchronized
     - all packages generated now are completely working and tested, unified in a single platform architecture and statically built
     - track library now correctly handle and parse - if  possible - already applied metadata from local files
     - fixed errors on downloading lyrics, due to too many concurrent requests
     - fixed errors on downloading artworks, due to too many concurrent requests
     - `-flush-metadata` (and eventually `-flush-missing`) are no more bugged as previously, and massively smarter
     - miscellaneous corrections

--- src/system/constants.go
+++ src/system/constants.go
@@ -10,7 +10,7 @@ var (
 )
 
 const (
-	VERSION            = 9
+	VERSION            = 10
 	VERSION_REPOSITORY = "https://github.com/streambinder/spotitube"
 	VERSION_ORIGIN     = "https://api.github.com/repos/streambinder/spotitube/releases/latest"
 	VERSION_URL        = VERSION_REPOSITORY + "/releases/latest"

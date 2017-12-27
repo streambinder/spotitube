bd90b8fca57267a890809b9470a32b3e8376a313
streambinder <posta@davidepucci.it>
2017-12-27 19:59:06 +0100

    version: increase to v11

--- src/system/constants.go
+++ src/system/constants.go
@@ -10,7 +10,7 @@ var (
 )
 
 const (
-	VERSION            = 10
+	VERSION            = 11
 	VERSION_REPOSITORY = "https://github.com/streambinder/spotitube"
 	VERSION_ORIGIN     = "https://api.github.com/repos/streambinder/spotitube/releases/latest"
 	VERSION_URL        = VERSION_REPOSITORY + "/releases/latest"
@@ -47,50 +47,50 @@ const (
 	SPOTIFY_HTML_TEMPLATE             = `<!DOCTYPE html>
 <html>
 <head>
-    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">
-    <title>SpotiTube</title>
-    <link rel="icon" href="` + SPOTIFY_FAVICON_URL + `" type="image/x-icon" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Condensed" />
-    <style>
-        body               { font: 20px 'Roboto Condensed', sans-serif; text-align: center; margin: 75px 0; padding: 50px; color: #333; border: solid 5px #1ED760; border-left: none; border-right: none; }
-        body *             { text-align: center; }
-        h1                 { font-size: 40px; text-transform: uppercase; }
-        h1>i.fa            { margin: auto 10px; }
-        article            { display: block; text-align: left; width: 650px; margin: 0 auto 50px; }
-        a                  { color: #dc8100; text-decoration: none; }
-        a:hover            { color: #333; text-decoration: none; }
-        p.timer            { font-size: 14px; color: #A0A0A0; text-align: center; text-transform: uppercase; }
-        div.signature      { border: 1px solid rgba(0, 0, 0, 0.05); border-radius: 5px; text-align: center; }
-        div.signature>img  { width: 35px; vertical-align: middle; }
-        div.signature>span { font-size: 15px; color: #505050; }
-    </style>
-    <script type="text/javascript">
-        var timeleft = ` + SPOTIFY_HTML_AUTOCLOSE_TIMEOUT + `;
-        var downloadTimer = setInterval(function() {
-            timeleft--;
-            document.getElementById("timer").textContent = timeleft;
-            if(timeleft <= 0)
-                clearInterval(downloadTimer);
-        }, 1000);
-        function setAutoClose() { window.setTimeout(autoClose, ` + SPOTIFY_HTML_AUTOCLOSE_TIMEOUT_MS + `); }
-        function autoClose() { window.close(); }
-    </script>
+	<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">
+	<title>SpotiTube</title>
+	<link rel="icon" href="` + SPOTIFY_FAVICON_URL + `" type="image/x-icon" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Condensed" />
+	<style>
+		body               { font: 20px 'Roboto Condensed', sans-serif; text-align: center; margin: 75px 0; padding: 50px; color: #333; border: solid 5px #1ED760; border-left: none; border-right: none; }
+		body *             { text-align: center; }
+		h1                 { font-size: 40px; text-transform: uppercase; }
+		h1>i.fa            { margin: auto 10px; }
+		article            { display: block; text-align: left; width: 650px; margin: 0 auto 50px; }
+		a                  { color: #dc8100; text-decoration: none; }
+		a:hover            { color: #333; text-decoration: none; }
+		p.timer            { font-size: 14px; color: #A0A0A0; text-align: center; text-transform: uppercase; }
+		div.signature      { border: 1px solid rgba(0, 0, 0, 0.05); border-radius: 5px; text-align: center; }
+		div.signature>img  { width: 35px; vertical-align: middle; }
+		div.signature>span { font-size: 15px; color: #505050; }
+	</style>
+	<script type="text/javascript">
+		var timeleft = ` + SPOTIFY_HTML_AUTOCLOSE_TIMEOUT + `;
+		var downloadTimer = setInterval(function() {
+			timeleft--;
+			document.getElementById("timer").textContent = timeleft;
+			if(timeleft <= 0)
+				clearInterval(downloadTimer);
+		}, 1000);
+		function setAutoClose() { window.setTimeout(autoClose, ` + SPOTIFY_HTML_AUTOCLOSE_TIMEOUT_MS + `); }
+		function autoClose() { window.close(); }
+	</script>
 </head>
 <body onLoad="setAutoClose()">
-    <article>
-        <h1><i class="fa fa-thumbs-up" aria-hidden="true"></i><br>%s</h1>
-        <div>
-            <h3>%s</h3>
-            <br><br><br>
-            <p class="timer">Window will attempt to close in <span id="timer">` + SPOTIFY_HTML_AUTOCLOSE_TIMEOUT + `</span> seconds.</p>
-            <br>
-            <div class="signature">
-                <img src="` + SPOTIFY_HTML_SIG_ICON + `"/>
-                <span>` + SPOTIFY_HTML_SIG_AUTHOR + `</span>
-            </div>
-        </div>
-    </article>
+	<article>
+		<h1><i class="fa fa-thumbs-up" aria-hidden="true"></i><br>%s</h1>
+		<div>
+			<h3>%s</h3>
+			<br><br><br>
+			<p class="timer">Window will attempt to close in <span id="timer">` + SPOTIFY_HTML_AUTOCLOSE_TIMEOUT + `</span> seconds.</p>
+			<br>
+			<div class="signature">
+				<img src="` + SPOTIFY_HTML_SIG_ICON + `"/>
+				<span>` + SPOTIFY_HTML_SIG_AUTHOR + `</span>
+			</div>
+		</div>
+	</article>
 </body>
 </html>`
 )

af52eb622adf294eb415bb1337778c32312b76e0
streambinder <posta@davidepucci.it>
2018-09-16 19:28:15 +0200

    version: increase to v18

--- src/system/constant.go
+++ src/system/constant.go
@@ -2,7 +2,7 @@ package system
 
 const (
 	// Version : current version
-	Version = 17
+	Version = 18
 	// VersionRepository : repositoy container
 	VersionRepository = "https://github.com/streambinder/spotitube"
 	// VersionOrigin : API repository latest version URL

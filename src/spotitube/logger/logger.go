6649ad34032513fb494b90d95b9684071f622d8e
streambinder <posta@davidepucci.it>
2019-06-02 17:52:05 +0200

    src: logger: run log call in background

--- src/spotitube/logger/logger.go
+++ src/spotitube/logger/logger.go
@@ -30,16 +30,19 @@ func Build(filename string) *Logger {
 
 // Append : make Logger object log input message string, eventually throwing a returning error
 func (logger *Logger) Append(message string) error {
-	logger.Mutex.Lock()
-	defer logger.Mutex.Unlock()
-	loggerFile, err := os.OpenFile(logger.File, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
-	if err != nil {
-		return err
-	}
-	defer loggerFile.Close()
-	if _, err = loggerFile.WriteString(fmt.Sprintf("[%s] %s\n", time.Now().Format("2006-01-02 15:04:05"),
-		vtclean.Clean(strings.Replace(message, "\n", " ", -1), false))); err != nil {
-		return err
-	}
+	go func() error {
+		logger.Mutex.Lock()
+		defer logger.Mutex.Unlock()
+		loggerFile, err := os.OpenFile(logger.File, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
+		if err != nil {
+			return err
+		}
+		defer loggerFile.Close()
+		if _, err = loggerFile.WriteString(fmt.Sprintf("[%s] %s\n", time.Now().Format("2006-01-02 15:04:05"),
+			vtclean.Clean(strings.Replace(message, "\n", " ", -1), false))); err != nil {
+			return err
+		}
+		return nil
+	}()
 	return nil
 }

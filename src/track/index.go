abaa61456d7e335752d0564aa4296a4129f4df4b
streambinder <posta@davidepucci.it>
2019-09-23 23:55:52 +0200

    src: track: index: add support for check mathing entries and renaming

--- src/track/index.go
+++ src/track/index.go
@@ -1,6 +1,7 @@
 package track
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 
@@ -10,7 +11,6 @@ import (
 // TracksIndex : Tracks index keeping ID - filename mapping and eventual filename links
 type TracksIndex struct {
 	Tracks map[string]string
-	Links  map[string][]string
 }
 
 var (
@@ -20,7 +20,7 @@ var (
 // Index triggers a path scan searching for media files
 // and populating a TracksIndex object in return
 func Index(path string) *TracksIndex {
-	i := TracksIndex{Tracks: make(map[string]string), Links: make(map[string][]string)}
+	i := TracksIndex{Tracks: make(map[string]string)}
 
 	go func() {
 		filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
@@ -28,9 +28,7 @@ func Index(path string) *TracksIndex {
 				return nil
 			}
 
-			if linkPath, err := os.Readlink(path); err == nil {
-				i.Links[linkPath] = append(i.Links[linkPath], path)
-			} else if filepath.Ext(path) == ".mp3" {
+			if filepath.Ext(path) == ".mp3" {
 				id := GetTag(path, ID3FrameSpotifyID)
 				if len(id) > 0 {
 					i.Tracks[id] = path
@@ -54,4 +52,21 @@ func IndexWait() {
 // Sync flushes tracks index object on disk at input passed path
 func (index *TracksIndex) Sync(path string) error {
 	return system.DumpGob(path, index)
+}
+
+// Match returns whether an index element referenced by input id matches with input filename
+func (index *TracksIndex) Match(id string, filename string) (string, bool, error) {
+	if path, ok := index.Tracks[id]; ok {
+		pathFilename := filepath.Base(path)
+		return pathFilename, pathFilename == filepath.Base(filename), nil
+	}
+
+	return "", false, fmt.Errorf(fmt.Sprintf("Element with key %s does not exist", id))
+}
+
+// Rename replace input id element with input filename
+func (index *TracksIndex) Rename(id string, filename string) {
+	if path, ok := index.Tracks[id]; ok {
+		index.Tracks[id] = path
+	}
 }

16937c493ed505bd4c73f1ba30878f8eae711013
streambinder <posta@davidepucci.it>
2019-09-25 22:23:58 +0200

    src: system: network: add Wget() function to fetch assets

--- src/system/network.go
+++ src/system/network.go
@@ -1,7 +1,9 @@
 package system
 
 import (
+	"io"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -20,4 +22,29 @@ func IsOnline() bool {
 	r, _ := http.NewRequest("GET", "http://clients3.google.com/generate_204", nil)
 	_, err := Client.Do(r)
 	return err == nil
+}
+
+// Wget performs a GET over a given asset URL writing it on a give filename
+func Wget(url, fname string) error {
+	if err := os.Remove(fname); FileExists(fname) && err != nil {
+		return err
+	}
+
+	file, err := os.Create(fname)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	res, err := Client.Get(url)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if _, err := io.Copy(file, res.Body); err != nil {
+		return err
+	}
+
+	return nil
 }

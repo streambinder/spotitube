751b7a6751b1d3f130397af5b44efc270471c137
streambinder <posta@davidepucci.it>
2020-01-02 18:58:42 +0100

    src: track: playlist: simpler handling of playlists content generation

--- src/track/playlist.go
+++ src/track/playlist.go
@@ -2,7 +2,7 @@ package track
 
 import (
 	"fmt"
-	"os"
+	"path/filepath"
 	"strconv"
 
 	"github.com/streambinder/spotitube/src/system"
@@ -18,15 +18,13 @@ type Playlist struct {
 // M3U returns the M3U-compliant representation of the playlist
 func (p *Playlist) M3U(prefix string) string {
 	content := "#EXTM3U\n"
-	for i := len(p.Tracks) - 1; i >= 0; i-- {
-		t := p.Tracks[i]
-		if system.FileExists(t.Filename()) {
-			content += fmt.Sprintf("#EXTINF:%s,%s\n%s%c%s\n",
+	for _, t := range p.Tracks {
+		fname := filepath.Join(prefix, t.Filename())
+		if system.FileExists(fname) {
+			content += fmt.Sprintf("#EXTINF:%s,%s\n%s\n",
 				strconv.Itoa(t.Duration),
-				t.Filename(),
-				prefix,
-				os.PathSeparator,
-				t.Filename())
+				t.Basename(),
+				fname)
 		}
 	}
 
@@ -36,18 +34,15 @@ func (p *Playlist) M3U(prefix string) string {
 // PLS returns the PLS-compliant representation of the playlist
 func (p *Playlist) PLS(prefix string) string {
 	content := fmt.Sprintf("[%s]\n", p.Name)
-	for i := len(p.Tracks) - 1; i >= 0; i-- {
-		t := p.Tracks[i]
-		iInverse := len(p.Tracks) - i
-		if system.FileExists(t.Filename()) {
-			content += fmt.Sprintf("File%s=%s%c%s\nTitle%s=%s\nLength%s=%s\n\n",
-				strconv.Itoa(iInverse),
-				prefix,
-				os.PathSeparator,
-				t.Filename(),
-				strconv.Itoa(iInverse),
-				t.Filename(),
-				strconv.Itoa(iInverse),
+	for i, t := range p.Tracks {
+		fname := filepath.Join(prefix, t.Filename())
+		if system.FileExists(fname) {
+			content += fmt.Sprintf("File%s=%s\nTitle%s=%s\nLength%s=%s\n\n",
+				strconv.Itoa(i+1),
+				fname,
+				strconv.Itoa(i+1),
+				t.Basename(),
+				strconv.Itoa(i+1),
 				strconv.Itoa(t.Duration))
 		}
 	}

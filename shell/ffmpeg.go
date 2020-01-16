fd7d53de1ef6990767d1d6ae524e6b04bfc9f878
streambinder <posta@davidepucci.it>
2020-01-16 15:57:39 +0100

    shell: ffmpeg: use named return values

--- shell/ffmpeg.go
+++ shell/ffmpeg.go
@@ -32,34 +32,26 @@ func (c FFmpegCommand) Exists() bool {
 }
 
 // Version returns the command installed version
-func (c FFmpegCommand) Version() string {
+func (c FFmpegCommand) Version() (version string) {
 	var (
-		cmdOut    bytes.Buffer
-		cmdErr    error
-		cmdReg    *regexp.Regexp
-		cmdRegStr = "\\d+\\.\\d+\\.\\d+"
+		cmdOut bytes.Buffer
+		cmdReg = regexp.MustCompile("\\d+\\.\\d+\\.\\d+")
 	)
 
 	cmd := exec.Command(c.Name(), []string{"-version"}...)
 	cmd.Stdout = &cmdOut
-	if cmdErr = cmd.Run(); cmdErr != nil {
-		return ""
-	}
-
-	if cmdReg, cmdErr = regexp.Compile(cmdRegStr); cmdErr != nil {
-		return ""
+	if err := cmd.Run(); err != nil {
+		return
 	}
 
 	return cmdReg.FindString(cmdOut.String())
 }
 
 // VolumeDetect returns the float64 volume detection value for a given filename
-func (c FFmpegCommand) VolumeDetect(filename string) (float64, error) {
+func (c FFmpegCommand) VolumeDetect(filename string) (volume float64, err error) {
 	var (
-		cmdOut    bytes.Buffer
-		cmdErr    error
-		cmdReg    *regexp.Regexp
-		cmdRegStr = `max_volume:\s(?P<max_volume>[\-\.0-9]+)\sdB`
+		cmdOut bytes.Buffer
+		cmdReg = regexp.MustCompile(`max_volume:\s(?P<max_volume>[\-\.0-9]+)\sdB`)
 	)
 
 	cmd := exec.Command(c.Name(), []string{
@@ -69,29 +61,24 @@ func (c FFmpegCommand) VolumeDetect(filename string) (float64, error) {
 		"-y", "null"}...)
 	cmd.Stdout = &cmdOut
 	cmd.Stderr = &cmdOut
-	if cmdErr = cmd.Run(); cmdErr != nil {
-		return 0.0, cmdErr
-	}
-
-	if cmdReg, cmdErr = regexp.Compile(cmdRegStr); cmdErr != nil {
-		return 0.0, cmdErr
+	if err = cmd.Run(); err != nil {
+		return
 	}
 
 	cmdRegMatch := cmdReg.FindStringSubmatch(cmdOut.String())
 	cmdRegMap := system.MapGroups(cmdRegMatch, cmdReg.SubexpNames())
 	if val, ok := cmdRegMap["max_volume"]; ok {
-		valFloat, _ := strconv.ParseFloat(val, 64)
-		return valFloat, nil
+		volume, _ = strconv.ParseFloat(val, 64)
+		return
 	}
 
 	return 0.0, fmt.Errorf("Max volume value not found")
 }
 
 // VolumeSet increases max volume value by a given delta for a fiven filename
-func (c FFmpegCommand) VolumeSet(delta float64, filename string) error {
+func (c FFmpegCommand) VolumeSet(delta float64, filename string) (err error) {
 	var (
 		cmdOut      bytes.Buffer
-		cmdErr      error
 		tmpFilename = fmt.Sprintf("/tmp/%s", filepath.Base(filename))
 	)
 
@@ -102,14 +89,14 @@ func (c FFmpegCommand) VolumeSet(delta float64, filename string) error {
 		"-y", tmpFilename}...)
 	cmd.Stdout = &cmdOut
 	cmd.Stderr = &cmdOut
-	if cmdErr = cmd.Run(); cmdErr != nil {
-		return cmdErr
+	if err = cmd.Run(); err != nil {
+		return
 	}
 
-	cmdErr = system.FileMove(tmpFilename, filename)
-	if cmdErr != nil {
-		return cmdErr
+	err = system.FileMove(tmpFilename, filename)
+	if err != nil {
+		return
 	}
 
-	return nil
+	return
 }

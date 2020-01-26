a554ab97cf9cea92a62c83c5ddce99f6d6d16f0d
streambinder <posta@davidepucci.it>
2020-01-26 12:06:57 +0100

    zeus: keys-inflate: don't let spacing screw up inflating

--- zeus/scripts/keys-inflate.sh
+++ zeus/scripts/keys-inflate.sh
@@ -1,8 +1,8 @@
 if [ -n "${SPOTIFY_ID}" ] || [ -n "${SPOTIFY_KEY}" ]; then
-    sed -i'.bak' 's|clientID = ""|clientID = "'"${SPOTIFY_ID}"'"|g;
-                  s|clientSecret = ""|clientSecret = "'"${SPOTIFY_KEY}"'"|g' spotify/api.go
+    sed -i'.bak' 's|clientID[[:space:]].*""|clientID = "'"${SPOTIFY_ID}"'"|g;
+                  s|clientSecret[[:space:]].*""|clientSecret = "'"${SPOTIFY_KEY}"'"|g' spotify/api.go
 fi
 
 if [ -n "${GENIUS_TOKEN}" ]; then
-    sed -i'.bak' 's|geniusToken = ""|geniusToken = "'"${GENIUS_TOKEN}"'"|g' lyrics/genius.go
+    sed -i'.bak' 's|geniusToken[[:space:]].*""|geniusToken = "'"${GENIUS_TOKEN}"'"|g' lyrics/genius.go
 fi

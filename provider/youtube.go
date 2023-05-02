df219b67bfda36f4fc676a67150053e9a40ddbf6
streambinder <posta@davidepucci.it>
2023-05-02 22:30:38 +0200

    feat(youtube): introduce skim threshold and compliancy checkers

--- provider/youtube.go
+++ provider/youtube.go
@@ -21,6 +21,8 @@ type youTube struct {
 }
 
 type youTubeResult struct {
+	track  *entity.Track
+	query  string
 	id     string
 	title  string
 	owner  string
@@ -31,7 +33,9 @@ type youTubeResult struct {
 const (
 	titleScoreMultiplier    = 1.5
 	durationScoreMultiplier = 2.0
-	viewsScoreMultiplier    = 0.000001
+	viewsScoreMultiplier    = 0.0000005
+	keywordsMatchScore      = 100
+	skimThreshold           = -100
 )
 
 func init() {
@@ -67,6 +71,8 @@ func (provider youTube) search(track *entity.Track) ([]*Match, error) {
 	var matches []*Match
 	gjson.Get(resultJSON, "contents.twoColumnSearchResultsRenderer.primaryContents.sectionListRenderer.contents.0.itemSectionRenderer.contents").ForEach(func(key, value gjson.Result) bool {
 		match := youTubeResult{
+			track: track,
+			query: searchKeys.Get("search_query"),
 			id:    gjson.Get(value.String(), "videoRenderer.videoId").String(),
 			title: gjson.Get(value.String(), "videoRenderer.title.runs.0.text").String(),
 			owner: gjson.Get(value.String(), "videoRenderer.ownerText.runs.0.text").String(),
@@ -93,22 +99,30 @@ func (provider youTube) search(track *entity.Track) ([]*Match, error) {
 			return true
 		}
 
-		matches = append(matches, &Match{
-			fmt.Sprintf("https://youtu.be/%s", match.id),
-			func(result youTubeResult) (score int) {
-				var (
-					titleScore = int(float64(levenshtein.ComputeDistance(
-						util.UniqueFields(searchKeys.Get("search_query")),
-						util.UniqueFields(fmt.Sprintf("%s %s", result.owner, result.title)),
-					)) * titleScoreMultiplier)
-					durationScore = int(math.Abs(float64(result.length)-float64(track.Duration)) * durationScoreMultiplier)
-					viewsScore    = int(float64(result.views) * viewsScoreMultiplier)
-				)
-				return viewsScore - titleScore - durationScore
-			}(match),
-		})
+		if match.score() > skimThreshold {
+			matches = append(matches, &Match{fmt.Sprintf("https://youtu.be/%s", match.id), match.score()})
+		}
+
 		return true
 	})
 
 	return matches, nil
+}
+
+func (result youTubeResult) score() int {
+	var (
+		queryDistinct  = util.UniqueFields(result.query)
+		resultDistinct = util.UniqueFields(fmt.Sprintf("%s %s", result.owner, result.title))
+		titleScore     = int(float64(levenshtein.ComputeDistance(queryDistinct, resultDistinct)) * titleScoreMultiplier)
+		durationScore  = int(math.Abs(float64(result.length)-float64(result.track.Duration)) * durationScoreMultiplier)
+		viewsScore     = int(float64(result.views) * viewsScoreMultiplier)
+		score          = viewsScore - titleScore - durationScore
+	)
+
+	if strings.Contains(resultDistinct, util.Flatten(result.track.Artists[0])) &&
+		strings.Contains(resultDistinct, util.Flatten(result.track.Title)) {
+		score += keywordsMatchScore
+	}
+
+	return score
 }

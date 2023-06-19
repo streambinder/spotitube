0d8384ea4d042dae1ab10ea7ae5e995b847f4274
streambinder <posta@davidepucci.it>
2023-06-19 12:12:47 +0200

    fix(genius): lookup matches for main artist only too

--- lyrics/genius.go
+++ lyrics/genius.go
@@ -18,6 +18,10 @@ import (
 	"github.com/streambinder/spotitube/util"
 )
 
+const contextValueLabelMainArtist = "mainArtistOnly"
+
+type contextValueLabel string
+
 type genius struct {
 	Composer
 }
@@ -31,6 +35,7 @@ type geniusSearch struct {
 }
 
 type geniusResult struct {
+	track  *entity.Track
 	query  string // not part of APIs
 	URL    string
 	Title  string
@@ -44,14 +49,23 @@ func init() {
 }
 
 func (composer genius) search(track *entity.Track, ctxs ...context.Context) ([]byte, error) {
-	ctx := context.Background()
+	var (
+		ctx            = context.Background()
+		mainArtistOnly = false
+	)
 	if len(ctxs) > 0 {
 		ctx = ctxs[0]
+	}
+	if ctxMainArtistOnly, ok := ctx.Value(contextValueLabel(contextValueLabelMainArtist)).(bool); ok {
+		mainArtistOnly = ctxMainArtistOnly
 	}
 
 	query := track.Song()
 	for _, artist := range track.Artists {
 		query = fmt.Sprintf("%s %s", query, artist)
+		if mainArtistOnly {
+			break
+		}
 	}
 
 	request, err := http.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("https://api.genius.com/search?q=%s", url.QueryEscape(query)), nil)
@@ -89,15 +103,19 @@ func (composer genius) search(track *entity.Track, ctxs ...context.Context) ([]b
 		url   string
 	)
 	for _, hit := range data.Response.Hits {
+		hit.Result.track = track
 		hit.Result.query = query
-		if hitScore := hit.Result.score(track); hit.Result.compliant(track) && hitScore > score {
+		if hitScore := hit.Result.score(); hit.Result.compliant() && hitScore > score {
 			url = hit.Result.URL
 			score = hitScore
 		}
 	}
 
-	if url == "" {
+	if url == "" && mainArtistOnly {
 		return nil, nil
+	} else if url == "" {
+		return composer.search(
+			track, context.WithValue(ctx, contextValueLabel(contextValueLabelMainArtist), true))
 	}
 
 	return composer.fromGeniusURL(url, ctx)
@@ -151,16 +169,16 @@ func documentParser(data *[]byte) func(i int, s *goquery.Selection) {
 
 // compliance check works as a barrier before checking on the result score
 // so to ensure that only the results that pass certain pre-checks get returned
-func (result geniusResult) compliant(track *entity.Track) bool {
+func (result geniusResult) compliant() bool {
 	spec := util.UniqueFields(fmt.Sprintf("%s %s", result.Artist.Name, result.Title))
 	return result.URL != "" &&
-		util.Contains(spec, strings.Split(util.UniqueFields(track.Artists[0]), " ")...) &&
-		util.Contains(spec, strings.Split(util.UniqueFields(track.Song()), " ")...)
+		util.Contains(spec, strings.Split(util.UniqueFields(result.track.Artists[0]), " ")...) &&
+		util.Contains(spec, strings.Split(util.UniqueFields(result.track.Song()), " ")...)
 }
 
 // score goes from 0 to 100 and it's built on the accuracy percentage
 // for result artist+title compared to the given track
-func (result geniusResult) score(track *entity.Track) int {
+func (result geniusResult) score() int {
 	distance := int(math.Min(
 		float64(levenshtein.ComputeDistance(
 			util.UniqueFields(result.query),

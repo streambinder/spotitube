25e20596a5f5f37e3ebaeb59efc65705521d9795
streambinder <posta@davidepucci.it>
2023-06-27 17:46:27 +0200

    chore(random): spare some variable

--- util/random.go
+++ util/random.go
@@ -12,8 +12,7 @@ func init() {
 
 func seed() {
 	var b [8]byte
-	_, err := crand.Read(b[:])
-	if err != nil {
+	if ErrOnly(crand.Read(b[:])) != nil {
 		panic("cannot seed math/rand package with cryptographically secure random number generator")
 	}
 	mrand.Seed(int64(binary.LittleEndian.Uint64(b[:])))

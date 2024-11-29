ee15ff9cbea38792954778cfda2900ef2e2e2046
streambinder <posta@davidepucci.it>
2024-11-29 17:33:10 +0100

    chore: lint

--- util/int.go
+++ util/int.go
@@ -5,7 +5,7 @@ import (
 	"math/big"
 )
 
-func RandomInt(max int, mins ...int) int {
-	min := First(mins, 0)
-	return int(ErrWrap(big.NewInt(0))(rand.Int(rand.Reader, big.NewInt(int64(max-min)))).Int64()) + min
+func RandomInt(upperbound int, lowerbounds ...int) int {
+	lowerbound := First(lowerbounds, 0)
+	return int(ErrWrap(big.NewInt(0))(rand.Int(rand.Reader, big.NewInt(int64(upperbound-lowerbound)))).Int64()) + lowerbound
 }
